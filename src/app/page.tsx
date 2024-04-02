b766de9d9070385524a736d50e764536c3962d19
Loic Mahieu <mahieuloic@gmail.com>
2024-04-02 17:23:30 +0200

    form page: style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,17 +8,16 @@ export default function Home() {
   const router = useRouter();
   return (
     <form
+      className="flex flex-col items-center justify-center h-screen"
       onSubmit={(e) => {
         e.preventDefault();
         const url = (e.target as any).url.value;
         router.push(`/viewer/${encodeURIComponent(url)}`);
       }}
     >
-      <div>
+      <div className="w-80">
         <Input type="text" name="url" required />
-      </div>
-      <div>
-        <Button type="submit" variant="outline">
+        <Button type="submit" variant="outline" className="mt-4 w-full">
           Submit
         </Button>
       </div>
