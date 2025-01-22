da82480523c7442edeeb70a388e993825ec836f5
Loic Mahieu <mahieuloic@gmail.com>
2025-01-22 16:46:04 +0100

    Update all dependencies

--- src/app/viewer/[url]/msg/page.tsx
+++ src/app/viewer/[url]/msg/page.tsx
@@ -2,11 +2,17 @@ import { Suspense } from "react";
 import { EmailParser } from "../EmailParser";
 import { LoaderView } from "@/components/LoaderView";
 
-export default function ViewerPage({
-  params: { url },
-}: {
-  params: { url: string };
-}) {
+export default async function ViewerPage(
+  props: {
+    params: Promise<{ url: string }>;
+  }
+) {
+  const params = await props.params;
+
+  const {
+    url
+  } = params;
+
   return (
     <Suspense fallback={<LoaderView />}>
       <EmailParser url={decodeURIComponent(url)} type="msg" />
