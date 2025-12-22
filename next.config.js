dfd67c12bccc5d0e141a670dc45fd0f328a8beeb
Florent Detry <florent@igloo.be>
2025-12-22 12:14:45 +0100

    Try other headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Headers removed to allow content to display properly
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors *; frame-src *; child-src *; default-src 'self' 'unsafe-inline' 'unsafe-eval' data: blob: *;",
+          },
+        ],
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
